baedce39b8394f1186566feeeed278c429514ae8
Diogo Pinto <thediogopinto@gmail.com>
2023-06-26 23:59:56 +0100

    Format code with Prettier

--- neto design/js/main.js
+++ neto design/js/main.js
@@ -1,25 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const expandIcons = document.querySelectorAll('.expand-icon');
-  
+
     expandIcons.forEach(function (icon) {
-      icon.addEventListener('click', function () {
-        const textContainer = this.parentElement.querySelector('.text-container');
-  
-        textContainer.classList.toggle('collapsed');
-        this.classList.toggle('collapsed');
-      });
+        icon.addEventListener('click', function () {
+            const textContainer = this.parentElement.querySelector('.text-container');
+
+            textContainer.classList.toggle('collapsed');
+            this.classList.toggle('collapsed');
+        });
     });
-  });
+});
 
-  const elements = document.querySelectorAll('.section, .container1, .container2');
+const elements = document.querySelectorAll('.section, .container1, .container2');
 
-  function animateElements() {
+function animateElements() {
     elements.forEach((element, index) => {
-      setTimeout(() => {
-        element.classList.add('show');
-      }, 200 * index); // Adjust the delay as per your preference
+        setTimeout(() => {
+            element.classList.add('show');
+        }, 200 * index); // Adjust the delay as per your preference
     });
-  }
-  
-  animateElements();
+}
 
+animateElements();
