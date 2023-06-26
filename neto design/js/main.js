699e33bb4d038dace27f53b8d44995f94d0f0ec8
AlexNeto/ <113767524+AlexNetoo@users.noreply.github.com>
2023-06-26 01:07:11 +0100

    Add files via upload

---
+++ neto design/js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const expandIcons = document.querySelectorAll('.expand-icon');
+  
+    expandIcons.forEach(function (icon) {
+      icon.addEventListener('click', function () {
+        const textContainer = this.parentElement.querySelector('.text-container');
+  
+        textContainer.classList.toggle('collapsed');
+        this.classList.toggle('collapsed');
+      });
+    });
+  });
+
+  const elements = document.querySelectorAll('.section, .container1, .container2');
+
+  function animateElements() {
+    elements.forEach((element, index) => {
+      setTimeout(() => {
+        element.classList.add('show');
+      }, 200 * index); // Adjust the delay as per your preference
+    });
+  }
+  
+  animateElements();
+
